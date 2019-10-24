8232790d553c48ba09ec477a0cf136a21cba2b5d
Brandon Ubben <brandon@ubben.co>
2019-10-24 14:56:23 -0700

    Complete re-do

--- js/index.js
+++ js/index.js
@@ -1,30 +1,12 @@
-function addToContainer(elem) {
-  const container = document.querySelector('.container--logos');
-  container.appendChild(elem);
-}
+import Typed from 'typed.js';
 
-function createImageDiv() {
-  const newDiv = document.createElement('div');
-  const newImg = new Image();
-  const randAnimationDuration = Math.round(Math.random() * 7) + 7;
+const options = {
+  autoInsertCss: true,
+  loop: true,
+  showCursor: false,
+  smartBackspace: true,
+  stringsElement: '.header__figure__caption__list',
+  typeSpeed: 69,
+};
 
-  newDiv.classList.add('scrolling-logo');
-  newImg.style.animation = `travel ${randAnimationDuration}s linear infinite alternate`;
-
-  newImg.src = 'https://images.ubben.co/logo.svg';
-  newImg.alt = 'UBBEN';
-
-  newDiv.appendChild(newImg);
-  addToContainer(newDiv);
-}
-
-function renderImgDivs() {
-  let i = 0;
-
-  while (i <= 16) {
-    createImageDiv();
-    i++;
-  }
-}
-
-renderImgDivs();
+const typed = new Typed('.header__figure__caption', options);
