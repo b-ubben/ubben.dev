aa59342ef9fbf8b43d231573b48b4f60b0b31787
Brandon Ubben <brandon@ubben.co>
2019-11-06 09:42:20 -0800

    Tweaked animations

--- js/gsap.js
+++ js/gsap.js
@@ -6,8 +6,10 @@ const tl = new TimelineMax();
 // elements
 const header = document.querySelector('.header');
 const terminal = document.querySelector('.header__figure');
+const container = document.querySelector('.container');
 
 // TL
-tl.add( TweenMax.from(header, 1, { y: '-100%', opacity: 0, ease: Expo.easeOut }) )
-  .add( TweenMax.from(terminal, .5, { x: '-100%', opacity: 0, ease: Expo.easeOut }) );
+tl.add( TweenMax.from(header, 1, { y: '-100%', opacity: 0, delay: .75, ease: Expo.easeOut }) )
+  .add( TweenMax.from(terminal, .5, { x: '-100%', opacity: 0, ease: Expo.easeOut }) )
+  .add( TweenMax.from(container, .5, { opacity: 0, delay: .25, ease: Expo.easeIn }) );
 
